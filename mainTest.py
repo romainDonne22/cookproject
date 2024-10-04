6327addb0a0cbd175b9383516d5879db2ba2eac2
romainDonne22 <rom.donne@gmail.com>
2024-10-04 16:39:42 +0200

    poetry à jour

--- mainTest.py
+++ mainTest.py
@@ -1,1 +1,8 @@
-print("test Romain")
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pandas as pd 
+import seaborn as sns 
+
+# Import the data and print the first three rows 
+recipe = pd.read_csv("../data/RAW_recipes.csv")
+print(recipe.head(3))
