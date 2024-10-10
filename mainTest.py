e6cb210ff476458a17d8e3af59b913c90254224d
romainDonne22 <rom.donne@gmail.com>
2024-10-10 14:00:22 +0200

    répartition des notes

--- mainTest.py
+++ mainTest.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import pandas as pd 
 import seaborn as sns 
-import ast
+import matplotlib
 
 def datasetAnalysis(data):
     print("-------------------")
@@ -33,31 +33,33 @@ def analyseStars(data):
     print("Stars Analyse ")
     print("-------------------")
     
-    # Convertir les valeurs de la colonne 'ratings' en listes de nombres
-    data['ratings'] = data['ratings'].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
-    
-    # Aplatir toutes les listes en une seule liste
-    all_ratings = [item for sublist in data['ratings'] for item in sublist]
-    
     # Calculer la moyenne totale des notes
-    nub_ratings = len(all_ratings)
-    nub_users = len(data['u'])
-    mean_ratings = sum(all_ratings) / nub_ratings
+    nub_ratings = len(data['rating'])
+    nub_users = len(data['user_id'])
+    mean_ratings = data['rating'].mean()
     print("Le nombre total d'utilisateurs est : ", nub_users)
     print("Le nombre total de notes est : ", nub_ratings)
     print("La moyenne totale des notes est : ", mean_ratings)
 
-    plt.figure()
-    plt.scatter(all_ratings, data['u'])
-    plt.title('Investment data')
-    plt.xlabel('Gross National Product')
-    plt.ylabel('Investment')
-    plt.show()
+    # Calculer la répartition des notes
+    ratings_count = data['rating'].value_counts().sort_index()
+
+    # Créer un DataFrame pour les notes
+    ratings_df = pd.DataFrame({'rating': ratings_count.index, 'count': ratings_count.values})
+
+    # Tracer un graphique en barres de la répartition des notes avec Seaborn
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='rating', y='count', data=ratings_df, palette='viridis', edgecolor='black')
+    plt.title('Répartition des notes')
+    plt.xlabel('Notes')
+    plt.ylabel('Nombre de personnes')
+    # Enregistrer le graphique dans un fichier
+    plt.savefig('repartition_des_notes.png')
 
 # Analyse the dataset RAW_recipes
 recipe = pd.read_csv("../data/RAW_recipes.csv")
 #datasetAnalysis(recipe)
 
 # Analyse the dataset PP_users
-datasetAnalysis(pd.read_csv("../data/PP_users.csv"))
-analyseStars(pd.read_csv("../data/PP_users.csv"))
+datasetAnalysis(pd.read_csv("../data/RAW_interactions.csv"))
+analyseStars(pd.read_csv("../data/RAW_interactions.csv"))
