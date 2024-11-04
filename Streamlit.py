be4a28d9dc9bf1dea6666b9da1d72ec5b502d387
CIS31 <cam-m-i@hotmail.fr>
2024-11-04 15:01:14 +0100

    new file:   Streamlit.py 	deleted:    poetry.lock 	deleted:    pyproject.toml

---
+++ Streamlit.py
@@ -0,0 +1,11 @@
+import streamlit as st
+#utiliser streamlit online pour l'url
+#fast api permet de pour le back et y a un template dans le got qui empact le fullstack 
+
+
+st.title('Hello, World!')
+
+
+with st.beta_expander("Dashboard"):
+    st.write("Contenu du dashboard ici")
+
