a76c4af93e12afae995ed7a9d3873873e14be30b
romainDonne22 <rom.donne@gmail.com>
2024-11-22 14:59:13 +0100

    test cleaned users_analysis

--- front.py
+++ front.py
@@ -72,12 +72,55 @@ def init_data_part2():
             'ingredients', 'n_ingredients', 'calories', 'total_fat',
             'sugar', 'sodium', 'protein', 'carbohydrates', 'year',
             'month', 'day', 'day_of_week']
-    return user_analysis
+    
+    numerical_col = user_analysis.select_dtypes(include=['int64', 'float64']).columns
+    # Calculer les pourcentages d'outliers pour chaque colonne :
+    outlier_info = {}
+    for column in numerical_col:  
+        Q1 = user_analysis[column].quantile(0.15)  
+        Q3 = user_analysis[column].quantile(0.85)  
+        IQR = Q3 - Q1  # Étendue interquartile
+    # Nous nous concentrerons sur la borne supérieure qui comprend tous les outliers
+    upper_bound = Q3 + 1.5 * IQR
+    # Identifier les outliers
+    outliers = user_analysis[(user_analysis[column] > upper_bound)]
+    
+    # Calculer le pourcentage d'outliers
+    outlier_percentage = (len(outliers) / len(user_analysis)) * 100
+    
+    # Ajouter les informations dans un dictionnaire
+    outlier_info[column] = {
+        'Upper Bound': upper_bound,
+        'Outlier Count': len(outliers),
+        'Outlier Percentage (%)': outlier_percentage
+    }
+    # Définir les colonnes sur lesquelles nous voulons appliquer la suppression des valeurs aberrantes
+    col_to_clean = ['minutes', 'n_steps', 'n_ingredients', 'calories', 'total_fat', 'sugar',
+                'sodium', 'protein', 'carbohydrates']
+    
+    # Créer une copie du DataFrame initial pour travailler dessus
+    cleaned_user_analysis = user_analysis.copy()
+
+    # Appliquer le filtrage des outliers
+    for col in col_to_clean:
+        Q1 = user_analysis[col].quantile(0.15)
+        Q3 = user_analysis[col].quantile(0.85)
+        IQR = Q3 - Q1  # Étendue interquartile
+        
+        # Calculer la borne supérieure
+        upper_bound = Q3 + 1.5 * IQR
+        
+        # Filtrer les lignes cumulativement
+        cleaned_user_analysis = cleaned_user_analysis[cleaned_user_analysis[col] <= upper_bound]
+    
+    
+
+    return user_analysis, cleaned_user_analysis
     
 def main():
     st.title("Analyse des mauvaises recettes") # Titre de l'application
     df, df_cleaned = init_data_part1() # Charger les données
-    user_analysis = init_data_part2()
+    user_analysis, cleaned_user_analysis = init_data_part2()
     st.sidebar.title("Navigation") # Titre de la sidebar
     choice = st.sidebar.radio("Allez à :", ["Introduction", "Caractéristiques des recettes mal notées", 
         "Influence du temps de préparation et de la complexité", "Influence du contenu nutritionnel", 
@@ -333,23 +376,9 @@ def main():
         st.write(user_analysis.head())
         nb_doublon=rrca.check_duplicates(user_analysis) # Vérifier les doublons
         st.write(f"Nombre de doublons : {nb_doublon}")
-        user_analysis = user_analysis.dropna(subset=['name']) # 34 notes ne correspondent à aucune recette. Ce sont les outliers qu'on a sorti du dataset recipe lors de la première analyse. Nous allons les drop.
-        user_analysis['review'] = user_analysis['review'].fillna("missing")
-        num_duplicates = user_analysis.duplicated().sum()
-        print(f"Nombre de doublons : {num_duplicates}")
-        # Nous ne gardons que les colonnes utiles à l'analyse et non répétitive
-        user_analysis.drop(['name', 'id','nutrition','steps', 'saturated fat (%)'], axis=1, inplace=True)
-        id_columns = ['recipe_id', 'user_id', 'contributor_id','year', 'month', 'day']
-        for col in id_columns:
-            user_analysis[col] = user_analysis[col].astype('object')
-        # Renaming des colonnes :
-        user_analysis.columns = ['user_id', 'recipe_id', 'date', 'rating', 'review', 'minutes',
-                'contributor_id', 'submitted', 'tags', 'n_steps', 'description',
-                'ingredients', 'n_ingredients', 'calories', 'total_fat',
-                'sugar', 'sodium', 'protein', 'carbohydrates', 'year',
-                'month', 'day', 'day_of_week']
+        
         display_fig(rrca.plot_distribution(user_analysis, 'rating', 'Distribution des notes'))
-
+        numerical_col_clean= cleaned_user_analysis.select_dtypes(include=['int64', 'float64']).columns
 
     # # Supprimer les colonnes en double
     # rrca.drop_columns(df, ['recipe_id', 'nutrition', 'steps']) 
